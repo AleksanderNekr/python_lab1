582f7ffb2133eb6dce11ffacf783dd6dfe858bd6
AleksanderNekr <aleksandr_nekrasov@outlook.com>
2022-01-25 20:33:34 +0500

    Some fixes and done task 4

--- task3_var6.py
+++ task3_var6.py
@@ -1,4 +1,4 @@
-import random
+from random import random
 
 
 def input_natural(msg):
@@ -21,14 +21,14 @@ def input_natural(msg):
 
 all_points_count = input_natural("Введите точность вычисления площади методом Монте-Карло (рекомендуется 100 000): ")
 square = 8 * 4
-shape_points_count = 0
-for i in range(all_points_count):
+correct_points_count = 0
+for _ in range(all_points_count):
     # координата x от 0 до 8
-    x = random.random() * 8
+    x = random() * 8
     # координата y от 4 до 8
-    y = random.random() * 4 + 4
+    y = random() * 4 + 4
     is_in_triangle = y <= x * 0.5 + 4
     is_in_rectangle = 5 <= x <= 7 and 5 <= y <= 6
     if is_in_triangle and not is_in_rectangle:
-        shape_points_count += 1
-print(f"Площадь примерно равна {square * shape_points_count / all_points_count}")
+        correct_points_count += 1
+print(f"Площадь примерно равна {square * correct_points_count / all_points_count}")
--- task4_var6.py
+++ task4_var6.py
@@ -1,3 +1,6 @@
+from itertools import product
+
+
 def input_natural(msg):
     """
     Ввод натурального числа
@@ -16,8 +19,43 @@ def input_natural(msg):
             print("Введено нецелое или не число!\nВведите заново: ", end="")
 
 
-a = int(input())
-b = int(input())
+def square_monte_carlo(X, Y, a, b, c, d, e):
+    square = X * max(Y, e)
+    correct_point_counts = 0
+    all_points_count = 100000
+    for _ in range(all_points_count):
+        from random import random
+        # координата x от 0 до max(Y, e)
+        x = random() * max(Y, e)
+        # координата y от 0 до X
+        y = random() * X
 
-for i in range(b - (b + 1) % 2, a - 1, -2):
-    print(i)
+        # Многоугольник разбивается на 3 прямоугольника:
+        # нижний (со сторонами e и d)
+        # средний (со сторонами Y - b и c)
+        # верхний (со сторонами Y и a)
+        is_in_bottom_square = x <= e and y <= d
+        is_in_middle_square = x <= Y - b and d <= y <= d + c
+        is_in_top_square = x <= Y and X - a <= y <= X
+        if is_in_top_square or is_in_middle_square or is_in_bottom_square:
+            correct_point_counts += 1
+    return square * correct_point_counts / all_points_count
+
+
+print("Принято ограничение на натуральные числа!")
+X = input_natural("Введите сторону Х: ")
+Y = input_natural("Введите сторону Y: ")
+Z = input_natural("Введите сторону Z: ")
+
+# Так как a + c + d = X и e - Z + b = Y, то e + b = Z + Y,
+# отсюда a + b + c + d + e = X + Y + Z = периметр фигуры
+perimeter = X + Y + Z
+
+iter_count = 0
+for e, b in product(range(1, round(Z + Y) - 1), range(1, round(Z + Y) - 1)):
+    if e + b == Z + Y:
+        for a, c, d in product(range(1, round(X) - 2), range(1, round(X) - 2), range(1, round(X) - 2)):
+            if a + c + d == X:
+                iter_count += 1
+                print(f"{iter_count}-й случай: a = {a}, b = {b}, c = {c}, d = {d}, e = {e},"
+                      f" площадь примерно равна: {square_monte_carlo(X, Y, a, b, c, d, e)}")
